857e182a0c7072be3817bb2d72b0e70029b14b8e
Paul Ronga <paul.ronga@laposte.net>
2022-05-12 14:45:42 +0200

    update folders & format

--- 01_preprocess/xlsx_to_csv.py
+++ 01_preprocess/xlsx_to_csv.py
@@ -5,23 +5,18 @@ reads csv much faster).
 
 import pandas as pd
 
-
 INPATH = "../data/"
 TO_CONVERT = ["votes", "votes_finaux"]
 OUTPATH = "../processed_data/"
 
-
-# Function to convert each file
-
 def xlsx_to_csv(filename):
+    '''Convert each file'''
     filepath = f"{INPATH}{filename}.xlsx"
+    print('Converting', filepath, '...')
     outpath = f"{OUTPATH}{filename}.csv"
-
     df = pd.read_excel(filepath)
-    df.to_csv(outpath, encoding='utf-8', header=True)
-
+    df.to_csv(outpath, encoding="utf-8", header=True, index=False)
 
 # Convert all the files specified in `TO_CONVERT`
-
 for filename in TO_CONVERT:
     xlsx_to_csv(filename)
