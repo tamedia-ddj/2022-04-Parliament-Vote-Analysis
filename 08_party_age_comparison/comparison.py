bdf348bacbc1b5c98e92ae29f7a711165e11c75d
Yann Aguettaz <yann@aguettaz.me>
2022-05-18 16:58:09 +0200

    Sort by median age

--- 08_party_age_comparison/comparison.py
+++ 08_party_age_comparison/comparison.py
@@ -3,10 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 parlementaires = pd.read_csv(f'../../parlement/donnees/parlementaires.csv', index_col=0).T
-parties = set(parlementaires['party'])
+parties = list(set(parlementaires['party']))
 
 ages = []
 data_parties = []
+
+def median(l):
+    l = sorted(l)
+    if len(l) % 2 == 1:
+        return l[len(l) // 2]
+    else:
+        return (l[len(l) // 2] + l[len(l) // 2 + 1]) // 2
 
 for party in parties:
     parlementaires_party = parlementaires[parlementaires['party'] == party]
@@ -18,6 +25,12 @@ for party in parties:
 
     ages.append(list(parlementaires_party['age']))
     data_parties.append(f'{party} ({len(parlementaires_party)})')
+
+parties_ages = list(zip(ages, data_parties))
+parties_ages.sort(key=lambda t: median(t[0]))
+
+ages = [pa[0] for pa in parties_ages]
+data_parties = [pa[1] for pa in parties_ages]
 
 fig, ax = plt.subplots()
 #ax.violinplot(tuple(ages), vert=False, widths=.7, quantiles=[[.25, .5, .75]]*len(ages))
