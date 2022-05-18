748873621a5d7992f09a6b72d58d0eda58b52449
Yann Aguettaz <yann@aguettaz.me>
2022-05-18 03:07:01 +0200

    Add nbs

--- 08_party_age_comparison/comparison.py
+++ 08_party_age_comparison/comparison.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-parlementaires = pd.read_csv(f'donnees/parlementaires.csv', index_col=0).T
+parlementaires = pd.read_csv(f'../donnees/parlementaires.csv', index_col=0).T
 parties = set(parlementaires['party'])
 
 ages = []
@@ -17,11 +17,11 @@ for party in parties:
     parlementaires_party['age'] = parlementaires_party['year'].apply(lambda year: 2022 - year)
 
     ages.append(list(parlementaires_party['age']))
-    data_parties.append(party)
+    data_parties.append(f'{party} ({len(parlementaires_party)})')
 
 fig, ax = plt.subplots()
-#ax.violinplot(tuple(ages), vert=False, widths=.7, quantiles=[[.25, .5, .75]]*len(ages))
-ax.violinplot(tuple(ages), vert=False, widths=.7, showmeans=True)
+ax.violinplot(tuple(ages), vert=False, widths=.7, quantiles=[[.25, .5, .75]]*len(ages))
+#ax.violinplot(tuple(ages), vert=False, widths=.7, showmeans=True)
 ax.set_xlabel('Age')
 ax.set_yticks(np.arange(1, len(data_parties) + 1), labels=data_parties)
 plt.show()
